bc316ee9f07a6454de9e6258c8c40ec83ea3845b
Tesmi-Devlop <sashdfr@gmail.com>
2023-08-25 15:41:06 +1000

    updated comment

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,7 +2,7 @@ import { MoonTrack } from "./types";
 
 /**
  * Loads the provided save file to be played back. 
- * The save is a StringValue normally stored in game.ServerStorage.MoonAnimator2Saves, but you'll need to store it elsewhere to play the sequence back on the client.
+ * The `save` is a StringValue normally stored in `game.ServerStorage.MoonAnimator2Saves`, but you'll need to store it elsewhere to play the sequence back on the client.
  */
 declare const CreatePlayer: (save: StringValue, root?: Instance) => MoonTrack;
 
